e274dc1ed94f95ccbb14e55d82a6b0743d39bb6d
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-03-19 00:02:21 -0400

    trying

--- opener.py
+++ opener.py
@@ -8,16 +8,15 @@ from threading import Timer
 current = datetime.today()
 
 #next_day_sametime = current.replace(day= current.day +  1, hour = 1, minute = 0, second = 0, microsecond = 0)
-next_day_sametime = current.replace(day= current.day, hour = current.hour, minute = 48, second = 0, microsecond = 0)
+next_day_sametime = current.replace(day= current.day, hour = current.hour, minute = 0, second = 0, microsecond = 0)
 change_in_time = next_day_sametime - current 
-print (current, change_in_time)
 
 
 secs = change_in_time.seconds + 1
 
 def open(): 
 	url = 'https://www.southwest.com/air/flight-status/index.html'
-	webbrowser.open_new_tab(url + 'doc/')  # Open URL in a new tab, if a browser window is already open.
+	webbrowser.open_new_tab(url)  # Open URL in a new tab, if a browser window is already open.
 	return 
 
 # Open URL in new window, raising the window if possible.
