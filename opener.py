ce74f63c203aee564016ae2637cb6bbfc4d9c637
Shamikh Hossain <shamikh.hossain@duke.edu>
2017-03-19 22:18:43 -0400

    figured out timing

--- opener.py
+++ opener.py
@@ -5,25 +5,25 @@ from threading import Timer
 # http://stackoverflow.com/questions/11523918/python-start-a-function-at-given-time
 # http://stackoverflow.com/questions/15088037/python-script-to-do-something-at-the-same-time-every-day
 
-current = datetime.today()
 
-#next_day_sametime = current.replace(day= current.day +  1, hour = 1, minute = 0, second = 0, microsecond = 0)
-next_day_sametime = current.replace(day= current.day, hour = current.hour, minute = 0, second = 0, microsecond = 0)
-change_in_time = next_day_sametime - current 
+# time is in 24-hour format
+current_time = datetime.today()
+check_in_time = current_time.replace(day= current_time.day, hour = current_time.hour, minute = 18, second = 0, microsecond = 0)
+change_in_time = check_in_time - current_time 
 
+print ('current_time date and time: ', current_time)
+print ('time remaining: ', change_in_time)
 
-secs = change_in_time.seconds + 1
+seconds = change_in_time.seconds + 1
 
 def open(): 
 	url = 'https://www.southwest.com/air/flight-status/index.html'
-	webbrowser.open_new_tab(url)  # Open URL in a new tab, if a browser window is already open.
-	return 
+	try:
+		webbrowser.open_new_tab(url)  # Open URL in a new tab, if a browser window is already open.
+	except: 
+		webbrowser.open_new(url)
+	return "Check in page opened!"
 
-# Open URL in new window, raising the window if possible.
-#webbrowser.open_new(url)
-
-#open(url)
-
-time_watch = Timer(secs, open)
+time_watch = Timer(seconds, open)
 time_watch.start()
-print ("Flight Checker Initialized")
+print ("Flight Checker Initialized", check_in_time)
